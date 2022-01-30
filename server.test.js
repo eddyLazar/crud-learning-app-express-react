30fa07d78d7238086f124b8621ab5f33560d6ea0
Eddy <eddy.lazar@gmail.com>
2022-01-30 23:59:21 +0200

    added tests for users api

--- server.test.js
+++ server.test.js
@@ -2,6 +2,16 @@ const request = require("supertest");
 const app = require("./server");
 // const { describe, expect, test } = require("@jest/globals");
 
+const userMatchingObject = expect.objectContaining({
+  id: expect.any(Number),
+  name: expect.any(String),
+  username: expect.any(String),
+  email: expect.any(String),
+  birthdate: expect.any(String),
+});
+
+let newUserId;
+
 describe("Testing Endpoints", () => {
   test("GET: /", async () => {
     const res = await request(app).get("/");
@@ -9,18 +19,57 @@ describe("Testing Endpoints", () => {
     expect(res.body).toEqual({ hello: "world" });
   });
 
-  test("GET: /", async () => {
+  test("GET: /users", async () => {
     const res = await request(app).get("/users");
     expect(res.statusCode).toEqual(200);
     const user = res.body[0];
-    expect(res.body[0]).toMatchObject(
-      expect.objectContaining({
-        id: expect.any(Number),
-        name: expect.any(String),
-        username: expect.any(String),
-        email: expect.any(String),
-        birthdate: expect.any(String),
-      })
-    );
+    expect(user).toMatchObject(userMatchingObject);
+  });
+
+  test("POST: /users", async () => {
+    const newUser = {
+      name: "Test User",
+      username: "testy",
+      email: "testy@test.com",
+      birthdate: "1992-30-06",
+      address: {
+        street: "Test street",
+        suite: "test",
+        city: "test",
+        zipcode: "test",
+        geo: {
+          lat: "-37.3159",
+          lng: "81.1496",
+        },
+      },
+      phone: "12321312312",
+      website: "test.org",
+      company: {
+        name: "Testing",
+        catchPhrase: "test verything",
+        bs: "test verything right",
+      },
+    };
+    const res = await request(app).post("/users").send(newUser);
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toMatchObject(newUser);
+    newUserId = 1;
+  });
+
+  test("GET: /users/{id}", async () => {
+    const res = await request(app).get("/users/1");
+    expect(res.statusCode).toEqual(200);
+    const user = res.body;
+    expect(user).toMatchObject(userMatchingObject);
+  });
+
+  test("PUT: /users/{id}", async () => {
+    const newName = "some new name";
+    const res = await request(app).put("/users/1").send({
+      name: newName,
+    });
+    expect(res.statusCode).toEqual(200);
+    const user = res.body;
+    expect(user).toHaveProperty("name", newName);
   });
 });
